6847d8744bbc3b986945c53cbf2acb51f269ed3d
Julio Rodriguez <juliodevelops@gmail.com>
2022-02-17 13:49:55 -0500

    Heroku server changes

--- backend/server.js
+++ backend/server.js
@@ -31,17 +31,17 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/aurea', {
 
 app.use('/api/uploads', uploadRouter);
 
-// app.use(express.static(path.join(__dirname, '/frontend')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+})
   
   app.use('/api/users', userRouter);
   
-  app.get('/', (req, res) => {
-    res.send('Server is ready');
-  });
+  // app.get('/', (req, res) => {
+  //   res.send('Server is ready');
+  // });
 
   app.use('/api/products', productRouter);
 
